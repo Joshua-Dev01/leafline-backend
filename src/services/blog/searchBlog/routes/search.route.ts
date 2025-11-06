0f148c23d4f7b21d096546a512decf3e0bb221c6
chigozie joshua <chigozieuwakwe7@gmail.com>
2025-11-07 00:19:35 +0100

    handle so blog errors

--- src/services/blog/searchBlog/routes/search.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import catchAsync from "../../../../utils/catchAsync";
-import { searchBlogs } from "../../searchBlog/controller/searchBlog.controller";
-
-const searchBlogRouter = express.Router();
-
-searchBlogRouter.get("/search/query", catchAsync(searchBlogs));
-
-export default searchBlogRouter;
