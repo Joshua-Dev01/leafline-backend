4def34fac2dcb262e940d21784d45736125f87df
chigozie joshua <chigozieuwakwe7@gmail.com>
2025-11-07 00:34:49 +0100

    analytics error change

--- src/services/analytics/routes/analytics.routes.ts
+++ src/services/analytics/routes/analytics.routes.ts
@@ -1,9 +1,9 @@
-// routes/analytics.route.ts
-import express from "express";
-import { getMonthlyBlogStats } from "../controller/analytics.controller";
+// // routes/analytics.route.ts
+// import express from "express";
+// import { getMonthlyBlogStats } from "../controller/analytics.controller";
 
-const AnalyticsRouter = express.Router();
+// const AnalyticsRouter = express.Router();
 
-AnalyticsRouter.get("/monthly", getMonthlyBlogStats); // GET /api/analytics/monthly
+// AnalyticsRouter.get("/monthly", getMonthlyBlogStats); // GET /api/analytics/monthly
 
-export default AnalyticsRouter;
+// export default AnalyticsRouter;
